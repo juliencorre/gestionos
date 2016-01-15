ec6506302e81c60573b39bdd2ebf914176a62557
julien <jcorre@gmail.com>
2016-01-15 20:21:26 +0100

    MAJ

---
+++ app/tache.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1) {
+    var Tache;
+    return {
+        setters:[],
+        execute: function() {
+            Tache = (function () {
+                function Tache(id, nom) {
+                    this.id = id;
+                    this.nom = nom;
+                }
+                return Tache;
+            })();
+            exports_1("Tache", Tache);
+        }
+    }
+});
+//# sourceMappingURL=tache.js.map
